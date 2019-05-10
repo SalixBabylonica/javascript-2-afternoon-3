6a97d531d6a58e0c81798c86bf90fd2a7076a9a9
JaidenSalixB <divine.aligned.mind@gmail.com>
2019-05-10 05:03:28 -0600

    afternoon-3

--- practice.js
+++ practice.js
@@ -28,11 +28,14 @@
 */
 
 // Code Here 
-
+function first(arr, cb){
+ return cb(arr[0])
+  
+}
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 
-first(names, function(firstName){
+var t = first(names, function(firstName){
   console.log('The first name in names is ' + firstName);
   return firstName;
 });
@@ -48,6 +51,9 @@ first(names, function(firstName){
 */
 
 //Code Here
+function last (arr,cb){
+return cb(arr[arr.length-1])
+}
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -66,6 +72,9 @@ last(names, function(lastName){
 */
 
 //Code Here
+function multiply(num1,num2,cb){
+  return cb(num1*num2)
+}
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
@@ -85,6 +94,16 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
+function contains(arr,name,cb){
+  for(let i = 0;i < arr.length; i++){
+    console.log(i, arr[i])
+    if(arr[i] === name){
+      return cb(true)
+    }
+  }
+
+  return cb(false)
+}
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -106,7 +125,15 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
-
+function uniq(arr, cb) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        arr.splice(j, 1)
+      }
+    }return cb
+  }
+}
 // Do not edit the code below.
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
@@ -123,7 +150,9 @@ uniq(names, function(uniqArr){
 */
 
 //Code Here 
-
+function each (name1,name2,cb){
+  // for() 
+}
 // Do not edit the code below.
 each(names, function(item, indice){
   console.log('The item in the ' + indice + ' position is ' + item)
